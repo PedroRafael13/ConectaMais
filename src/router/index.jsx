795fcf80027dff11c031964134fabaafffa26b0d
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2025-10-15 17:15:03 -0300

    att

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/home/home";
-import App from "../App";
+//import App from "../App";
 import { Services } from "../pages/services/services";
 import  AgentConcierge  from "../pages/agentConcierge/index";
 import { Contract } from "../pages/contract/contract";
@@ -9,8 +9,7 @@ export function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/agentConcierge" element={<AgentConcierge />} />
         <Route path="/contact" element={<Contract />} />
